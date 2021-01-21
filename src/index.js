e6206a256b9313babc893f34b15df372d972511f
Kevin Burk <percburk@gmail.com>
2021-01-21 14:24:36 -0600

    completed reducers

--- src/index.js
+++ src/index.js
@@ -3,4 +3,53 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// redux plus logger
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// list of reducers
+const pizzaReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_PIZZA':
+      return [...state, action.payload];
+    case 'REMOVE_PIZZA':
+      let id = action.payload.id;
+      return state.filter((entry) => entry.id !== id);
+    default:
+      return state;
+  }
+};
+
+const infoReducer = (
+  state = {
+    customer_name: '',
+    street_address: '',
+    city: '',
+    zip: '',
+    type: '',
+  }
+) => {
+  switch (action.type) {
+    case 'SET_NEW_INFO':
+      return action.type.payload;
+    default:
+      return state;
+  }
+};
+
+// store
+const storeInstance = createStore(
+  combineReducers({
+    pizzaReducer,
+    infoReducer,
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
