11ec751a6bdfa53b26d7488f8117e1d0ee34b9b3
Kevin Burk <percburk@gmail.com>
2021-01-21 14:56:22 -0600

    completed router on App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Checkout from '../Checkout/Checkout';
+import Home from '../Home/Home';
+import Info from '../Info/Info';
+import Admin from '../Admin/Admin';
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Prime Pizza</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/info">Customer Info</Link>
+              </li>
+              <li>
+                <Link to="/checkout">Checkout</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Route path="/" exact component={Home} />
+        <Route path="/info" component={Info} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/admin" component={Admin} />
+      </div>
+    </Router>
   );
 }
 
