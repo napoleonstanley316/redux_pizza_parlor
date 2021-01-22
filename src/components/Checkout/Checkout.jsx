4220f7a922b80fbf29051a6eef88d6d2fd0e48d4
Woody Kromar <wkromar12@gmail.com>
2021-01-22 10:07:00 -0600

    confirm

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,20 +1,33 @@
-
 import Info from '../Info/Info.jsx';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux'
 
 
 function Checkout() {
-  
-  const infoReducer = useSelector((state) => state.infoReducer);
-  const pizzaReducer = useSelector((state) => state.pizzaReducer);
 
-  const handleCheckout = () => {
+    const infoReducer = useSelector(state => state.infoReducer)
+
+    const [checkoutList, setCheckoutList] = useState([]);
+  
+    
+    const handleCheckout = () => {
       console.log('handleCheckout clicked!');
+      axios({
+        method: 'POST',
+        url: '/api/order'
+      }).then((response) =>{
+        console.log('sending order details', response);
+        setCheckoutList(infoReducer.data)
+          console.log(checkoutList);
+
+      }).catch(function (error) {
+        console.log('Error on get:', error);
+      });
   }
 
-  console.log("In checkout");
+    
+    console.log('In checkout');
+return (
 
-  return (
     <section>
       <h2>Order Information</h2>
       <p>{infoReducer.customer_name}</p>
@@ -25,23 +38,16 @@ function Checkout() {
       <p>{infoReducer.total}</p>
       <p>{infoReducer.time}</p>
       <table>
-
         <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Cost</th>
-          </tr>
-          <tr>
-              <td>{pizzaReducer.name}</td>
-              <td>{pizzaReducer.price}</td>
-          </tr>
-          <button onClick={handleCheckout}>Checkout</button>
+            
         </tbody>
-
-
       </table>
     </section>
-  );
+
+)
+
+
 }
 
-export default Checkout;
+
+export default Info;
